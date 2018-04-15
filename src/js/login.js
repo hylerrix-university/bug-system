60cdc90899fb44a10aaef619d3c1fce97f0ec271
hylerrix <hylerrix@foxmail.com>
2018-04-16 01:08:04 +0800

    :beers: Writing code sleppy

--- src/js/login.js
+++ src/js/login.js
@@ -18,9 +18,34 @@ function doLogin () {
     }, function (result) {
         if (result.code == 200) {
             localStorage.setItem('isLogin', 'true')
+
+            // 存储用户个人信息
             localStorage.setItem('username', userName)
             localStorage.setItem('password', password)
-            window.location.href = './index.html'
+            localStorage.setItem('userId', result.data.userId)
+            localStorage.setItem('name', result.data.name)
+            localStorage.setItem('role', result.data.role)
+            localStorage.setItem('department', result.data.department)
+            localStorage.setItem('createTime', result.data.createTime)
+            localStorage.setItem('email', result.data.email)
+
+            // 存储用户负责的缺陷
+            $.get('http://yiranblade.cn/bugsystem/bug/user/' + result.data.userId, function (data) {
+                if (data.code == 200 ){
+                    localStorage.setItem('userBug', data.data)
+                    // 存储用户负责的项目
+                    $.get('http://yiranblade.cn/bugsystem/project/' + username, function (data) {
+                        if (data.code == 200 ){
+                            localStorage.setItem('userProject', data.data)
+                            window.location.href = './index.html'
+                        } else {
+                            alert('获取用户负责的项目失败')
+                        }
+                    })
+                } else {
+                    alert('获取用户负责的缺陷失败')
+                }
+            })
         } else {
             alert("账号密码输入错误！")
         }
